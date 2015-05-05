35cd1f68439f6971d5421b12cba73164ee99da69
green <green@cganalytics.org>
2015-05-04 22:26:53 -0700

    slow recursive solution to break rings exercise

---
+++ break-rings.py
@@ -0,0 +1,17 @@
+def break_rings(rings):
+    if not rings:
+        return 0
+    else:
+        remaining = []
+        for ring in sorted(reduce(set.union, rings)):
+            cost = break_rings([link for link in rings if ring not in link])
+            remaining.append(cost)
+        return 1 + min(remaining)
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert break_rings(({1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {4, 6})) == 3, "example"
+    assert break_rings(({1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4})) == 3, "All to all"
+    assert break_rings(({5, 6}, {4, 5}, {3, 4}, {3, 2}, {2, 1}, {1, 6})) == 3, "Chain"
+    assert break_rings(({8, 9}, {1, 9}, {1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}, {8, 7})) == 5, "Long chain"
+
