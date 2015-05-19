bcb7dde52c4e0000bd15802bf5a62c2df8e4f87e
green <green@cganalytics.org>
2015-05-19 16:10:11 -0700

    matrix pattern exercise

---
+++ matrix-pattern.py
@@ -0,0 +1,63 @@
+# You should write a program to search a binary matrix (a pattern)
+# within another binary matrix (an image). The recognition process
+# consists of scanning the image matrix row by row (horizontal scanning)
+# and when a pattern is located on the image, the program must mark this
+# pattern. To mark a located pattern change 1 to 3 and 0 to 2. The
+# result will be the image matrix with the located patterns marked.
+# The patterns in the image matrix are not crossed, because you should
+# immediately mark the pattern.
+# Input: Two arguments. A pattern as a list of lists and an image as a
+# list of lists.
+# Output: The marked image as a matrix as a list of list.
+
+def checkio(pattern, image):
+    i_height = len(image)
+    i_width = len(image[0])
+    p_height = len(pattern)
+    p_width = len(pattern[0])
+    for i in range(i_height-p_height+1):
+        for j in range(i_width-p_width+1):
+            if all(image[i+n][j+m] == pattern[n][m] for n in range(p_height) for m in range(p_width)):
+                for n in range(p_height):
+                    for m in range(p_width):
+                        image[i+n][j+m] += 2
+    return image
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([[1, 0], [1, 1]],
+                   [[0, 1, 0, 1, 0],
+                    [0, 1, 1, 0, 0],
+                    [1, 0, 1, 1, 0],
+                    [1, 1, 0, 1, 1],
+                    [0, 1, 1, 0, 0]]) == [[0, 3, 2, 1, 0],
+                                          [0, 3, 3, 0, 0],
+                                          [3, 2, 1, 3, 2],
+                                          [3, 3, 0, 3, 3],
+                                          [0, 1, 1, 0, 0]]
+    assert checkio([[1, 1], [1, 1]],
+                   [[1, 1, 1],
+                    [1, 1, 1],
+                    [1, 1, 1]]) == [[3, 3, 1],
+                                    [3, 3, 1],
+                                    [1, 1, 1]]
+    assert checkio([[0, 1, 0], [1, 1, 1]],
+                   [[0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+                    [0, 1, 1, 1, 0, 0, 0, 1, 1, 1],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 1, 1, 1, 0, 1, 0],
+                    [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+                    [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+                    [0, 1, 1, 0, 0, 0, 1, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]) == [[0, 2, 3, 2, 0, 0, 0, 2, 3, 2],
+                                                         [0, 3, 3, 3, 0, 0, 0, 3, 3, 3],
+                                                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                                         [0, 0, 0, 0, 2, 3, 2, 0, 0, 0],
+                                                         [2, 3, 2, 0, 3, 3, 3, 0, 1, 0],
+                                                         [3, 3, 3, 0, 0, 0, 0, 0, 1, 1],
+                                                         [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                                                         [0, 0, 1, 0, 0, 0, 2, 3, 2, 0],
+                                                         [0, 1, 1, 0, 0, 0, 3, 3, 3, 0],
+                                                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
